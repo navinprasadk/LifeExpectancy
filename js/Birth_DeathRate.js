353bf51e1624828279cdf4218a229cdb7b88152b
Your Name <you@example.com>
2017-01-30 07:00:24 +0000

    adding comments

--- js/Birth_DeathRate.js
+++ js/Birth_DeathRate.js
@@ -1,61 +1,59 @@
+module.exports = function LifeExpectancy_Male_Female(yyyy){
+  if(!yyyy)
+  {
+    throw new Error('Not a number');
+  }
+  if(isNaN(yyyy))
+  {
+    throw new Error('Not a number');
+  }
+
 let fs = require('fs');
 let re = require('readline');
 let lineReader = re.createInterface({
     input: fs.createReadStream('../inputdata/Indicators.csv')
 });
-
-/* insializing variables*/
-// let indexCountry = '';
-// let indexindicatorcode = '';
-// let indexyear = 0;
-// let indexvalue = 0;
-// let count = 0;
+// Initializing variables
 let output2 = [];
-
 let brate = 0;
 let drate = 0;
 let year = 1960;
-// let object = {};
 let object2 = {};
 let asiancountry = ['China', 'India', 'Pakistan', 'Singapore', 'Sri Lanka',
  'Thailand', 'Philippines', 'Israel', 'Indonesia', 'Iraq'];
-// let le = new Array('SP.DYN.LE00.FE.IN', 'SP.DYN.LE00.MA.IN', 'SP.DYN.LE00.IN');
 let bdrate = ['SP.DYN.CBRT.IN', 'SP.DYN.CDRT.IN'];
 
-// Handle stream events --> data, end, and error
 lineReader.on('line', function(chunk) {
+
     let split2 = chunk.trim().split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-    // console.log(split2[5])
+    //  To get the birth rate
     for (let i = year; i < 2016; i = i + 1) {
-        // console.log(asiancountry[i]);
         if (asiancountry[1] === split2[0] && bdrate[0] === split2[3]) {
             brate = Math.round(split2[5], 10);
-            //  console.log(sum);
         }
     }
     // To get the Death rate
     for (let i = year; i < 2016; i = i + 1) {
-        //  console.log('program started');
         if (asiancountry[1] === split2[0] && bdrate[1] === split2[3]) {
             drate = Math.round(split2[5], 10);
-            //  console.log(sum1);
         }
     }
-
     if (year < split2[4] && (bdrate[0] === split2[3] || bdrate[1] === split2[3]) && year <= 2016) {
         object2 = {
             'year': year,
             'Birth rate, crude (per 1,000 people)': brate,
             'Death rate, crude (per 1,000 people)': drate
         };
+        // Pushing the data
         output2.push(object2);
         brate = 0;
         drate = 0;
         year = year + 1;
     }
 });
+// Write the data on file
 lineReader.on('close', function() {
     fs.writeFile('../outputdata/OutputJSONNavin2.json', JSON.stringify(output2));
-    // console.log(output2);
-    //   console.log("Birth_Death_Rate.json was created")
 });
+return 'JSON written successfully';
+}
--- js/LifeExpectancy_Total.js
+++ js/LifeExpectancy_Total.js
@@ -1,44 +1,33 @@
+module.exports = function LifeExpectancy_Male_Female(yyyy){
+  if(!yyyy)
+  {
+    throw new Error('Not a number');
+  }
+  if(isNaN(yyyy))
+  {
+    throw new Error('Not a number');
+  }
 let fs = require('fs');
 let re = require('readline');
 let lineReader = re.createInterface({
     input: fs.createReadStream('../inputdata/Indicators.csv')
 });
-// var data = '';
-
-/* insializing variables*/
-// let indexCountry = '';
-// let indexindicatorcode = '';
-// let indexyear = 0;
-// let indexvalue = 0;
-// let count = 0;
-//
+// Initializing variables
 let output3 = [];
-// let output2 = [];
 let total = 0;
-// let sum1 = 0;
-// let brate = 0;
-// let drate = 0;
 let year = 1960;
 let object3 = {};
-// let object2 = {};
 let asiancountry = ['China', 'India', 'Pakistan', 'Singapore', 'Thailand'];
 let le = ['SP.DYN.LE00.FE.IN', 'SP.DYN.LE00.MA.IN', 'SP.DYN.LE00.IN'];
-// let bd_rate = new Array('SP.DYN.CBRT.IN', 'SP.DYN.CDRT.IN');
-
-// Handle stream events --> data, end, and error
 lineReader.on('line', function(chunk)
 {
     let split2 = chunk.trim().split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-    // console.log(split2[5]);
-
     // To get the total life expectancy
     for (let i = 0; i < asiancountry.length; i = i + 1) {
-        //  console.log('program started');
         for(let j = year; j < 2016; j = j + 1)
         {
             if (asiancountry[i] === split2[0] && le[2] === split2[3] && j === split2[4]) {
               total = total + Math.round(split2[5]);
-            //  console.log(sum1);
              }
       }
           if(i < asiancountry.length)
@@ -47,9 +36,15 @@ lineReader.on('line', function(chunk)
                 'year': asiancountry[i],
                 'Highest Life expectancy at birth': total
             }; i = i + 1;
+            // Pushing the data
             output3.push(object3);
-             total = 0;}
+             total = 0;
+           }
     }
-}); lineReader.on('close', function() {
+});
+// Write the data on file
+ lineReader.on('close', function() {
   fs.writeFile('../outputdata/OutputJSONNavin3.json', JSON.stringify(output3));
 });
+return 'JSON written successfully';
+}
